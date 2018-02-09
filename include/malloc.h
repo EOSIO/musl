55cc056291782e917abe3329142ad2b6ef3f6909
Anton Perkov <anton.perkov@block.one>
2018-02-09 14:41:08 -0500

    memcpy/memmove/memset removed (will use intrinsics) headers cleanup - removed functions for which we have no implementations removed time-related functions strftime implementation patched

--- include/malloc.h
+++ include/malloc.h
@@ -13,12 +13,7 @@ void *malloc (size_t);
 void *calloc (size_t, size_t);
 void *realloc (void *, size_t);
 void free (void *);
-   /*
-void *valloc (size_t);
-void *memalign(size_t, size_t);
 
-size_t malloc_usable_size(void *);
-   */
 #ifdef __cplusplus
 }
 #endif
