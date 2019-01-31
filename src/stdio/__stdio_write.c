601c6aaa41382e2efd768c1ab4f3285c5a1f261c
Bucky Kittinger <larrykittinger@gmail.com>
2019-01-31 18:18:58 -0500

    add prints_l stub

--- src/stdio/__stdio_write.c
+++ src/stdio/__stdio_write.c
@@ -1,7 +1,8 @@
 #include "stdio_impl.h"
 //include <sys/uio.h>
-#include <eosiolib/print.h>
 
+__attribute__((eosio_wasm_import))
+extern void prints_l(const char*, size_t);
 size_t __stdio_write(FILE *f, const unsigned char *buf, size_t len)
 {
    prints_l((char*)(f->wbase), f->wpos-f->wbase);
