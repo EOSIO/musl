b44c3999e45ae2a2e3ace4e9c003d42dab0361d7
Jeeyong Um <conr2d@gmail.com>
2019-08-03 07:57:30 +0000

    merge errno

--- src/errno/strerror.c
+++ src/errno/strerror.c
@@ -33,4 +33,11 @@ char *strerror(int e)
 	return __strerror_l(e, CURRENT_LOCALE);
 }
 
+#ifdef __APPLE__
+char *strerror_l(int e, locale_t loc)
+{
+	return __strerror_l(e,loc);
+}
+#else
 weak_alias(__strerror_l, strerror_l);
+#endif
