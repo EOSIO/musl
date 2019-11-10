e8ca983e694228df1280a6ab6b8e0ff97176f4d2
Jeeyong Um <conr2d@gmail.com>
2019-11-10 04:26:53 +0000

    remove redundant type conversion

--- src/time/time.c
+++ src/time/time.c
@@ -6,13 +6,7 @@ extern uint64_t current_time();
 
 time_t time(time_t *t)
 {
-	/*
-	struct timespec ts;
-	__clock_gettime(CLOCK_REALTIME, &ts);
-	if (t) *t = ts.tv_sec;
-	return ts.tv_sec;
-	*/
-	uint32_t elapsed = (uint32_t)(current_time() / 1000000ll);
+	time_t elapsed = (time_t)(current_time() / 1000000ll);
 	if (t) *t = elapsed;
 	return elapsed;
 }
