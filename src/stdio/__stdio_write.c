b05bfa9b732dfbfb2c6daea084a9153b15126ff8
Todd Fleming <todd.fleming@block.one>
2020-04-27 20:09:54 -0400

    moving tester to eos repo

--- src/stdio/__stdio_write.c
+++ src/stdio/__stdio_write.c
@@ -1,15 +1,15 @@
 #include "stdio_impl.h"
 //include <sys/uio.h>
 
-__attribute__((eosio_wasm_import)) int write_file(int fd, char* begin, char* end);
+__attribute__((eosio_wasm_import)) int write_file(int fd, char* begin, uint32_t size);
 
 __attribute__((eosio_wasm_import)) void printi(long long);
 __attribute__((eosio_wasm_import)) void prints_l(char*, int);
 
 size_t __stdio_write(FILE *f, const unsigned char *buf, size_t len)
 {
-   write_file(f->fd, (char*)f->wbase, (char*)f->wpos);
-   write_file(f->fd, (char*)buf, (char*)buf + len);
+   write_file(f->fd, (char*)f->wbase, (char*)f->wpos - (char*)f->wbase);
+   write_file(f->fd, (char*)buf, len);
 
    f->wend = f->buf + f->buf_size;
    f->wpos = f->wbase = f->buf;
--- src/stdio/fopen.c
+++ src/stdio/fopen.c
@@ -2,7 +2,7 @@
 #include <string.h>
 #include <errno.h>
 
-__attribute__((eosio_wasm_import)) int open_file(const char* pathname, const char* pathname_end, const char* mode, const char* mode_end);
+__attribute__((eosio_wasm_import)) int open_file(const char* pathname, uint32_t pathname_size, const char* mode, uint32_t mode_size);
 __attribute__((eosio_wasm_import)) void close_file(int fd);
 
 FILE *fopen(const char *restrict filename, const char *restrict mode)
@@ -20,7 +20,7 @@ FILE *fopen(const char *restrict filename, const char *restrict mode)
 	/* Compute the flags to pass to open() */
 	//flags = __fmodeflags(mode);
 
-	fd = open_file(filename, filename+strlen(filename), mode, mode+strlen(mode));
+	fd = open_file(filename, strlen(filename), mode, strlen(mode));
 	if (fd < 0) return 0;
 
 	f = __fdopen(fd, mode);
