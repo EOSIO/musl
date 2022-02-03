d2fa3c2c44ece4ace5c5b596bc8c8853af30d158
Huang-Ming Huang <huangh@objectcomputing.com>
2022-02-03 16:14:27 +0000

    fix __pthread_self()

--- arch/eos/pthread_arch.h
+++ arch/eos/pthread_arch.h
@@ -2,8 +2,4 @@
 
 typedef struct pthread* pthread_t;
 
-static inline struct pthread *__pthread_self()
-{
-   static struct pthread self = { 0 };
-   return &self;
-}
+struct pthread *__pthread_self();
